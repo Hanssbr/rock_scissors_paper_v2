6c452259fd18234f4aabf4de44c3907bf218b714
Hanssbr <rayhaniqbalbr@gmail.com>
2025-05-25 11:28:58 +0700

    update V2.2.2

--- index.js
+++ index.js
@@ -23,27 +23,46 @@ document.getElementById("gunting").onclick = function () {
 };
 
 document.getElementById("batu").onclick = function () {
-  bot();
-  if (botParse == "gunting") {
-    window.alert(`You win!, You choose Rock and bot choose Scissors`);
-  } else if (botParse == "batu") {
-    window.alert(`You tie!, You choose Rock and bot choose Rock`);
-  } else {
-    window.alert(`You lose!, You choose Rock and bot choose Paper`);
-  }
+  document.getElementById("loader").style.display = "block";
+  document.getElementById("loading").style.display = "block";
+  document.getElementById("loading").textContent = "Loading...";
+  setTimeout(function () {
+    document.getElementById("loading").textContent = "Data Loaded";
+    document.getElementById("loader").style.display = "none";
+  }, 1600);
+  setTimeout(function () {
+    bot();
+    if (botParse == "gunting") {
+      window.alert(`You win!, You choose Rock and bot choose Scissors`);
+    } else if (botParse == "batu") {
+      window.alert(`You Tie!, You choose Rock and bot choose Rock`);
+    } else {
+      window.alert(`You lose!, You choose Rock and bot choose Paper`);
+    }
+    document.getElementById("loading").style.display = "none";
+  }, 2000);
 };
 
-document.getElementById("kertas").onclick = function kertas() {
-  bot();
-  if (botParse == "gunting") {
-    window.alert(`You lose!, You choose Paper and bot choose Scissors`);
-  } else if (botParse == "batu") {
-    window.alert(`You win!, You choose Paper and bot choose Rock`);
-  } else {
-    window.alert(`You tie!, You choose Paper and bot choose Paper`);
-  }
+document.getElementById("kertas").onclick = function () {
+  document.getElementById("loader").style.display = "block";
+  document.getElementById("loading").style.display = "block";
+  document.getElementById("loading").textContent = "Loading...";
+  setTimeout(function () {
+    document.getElementById("loading").textContent = "Data Loaded";
+    document.getElementById("loader").style.display = "none";
+  }, 1600);
+  setTimeout(function () {
+    bot();
+    if (botParse == "gunting") {
+      window.alert(`You lose!, You choose Paper and bot choose Scissors`);
+    } else if (botParse == "batu") {
+      window.alert(`You win!, You choose Paper and bot choose Rock`);
+    } else {
+      window.alert(`You tie!, You choose Paper and bot choose Paper`);
+    }
+    document.getElementById("loading").style.display = "none";
+  }, 2000);
 };
-setTimeout(kertas, 5000);
 
 function bot() {
   let botValue;
